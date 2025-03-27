e54dd58d601c07e4e6285f85484ac33359d2b3fc
이호진 <skwo777@naver.com>
2025-03-28 01:56:58 +0900

    feat: 파이어베이스 init

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import Router from "./routes/Router";
 import { Global } from "@emotion/react";
-
 import GlobalStyles from "./components/styles/GlobalStyles";
+import Router from "./routes/Router";
 import StyleProvider from "./context/ContextProvider";
+
+// FB
+import "./firebase";
 
 function App() {
   return (

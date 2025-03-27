9910f8d90303a2db754429bb261d045b45bd0fc0
이호진 <skwo777@naver.com>
2025-03-28 02:30:23 +0900

    fix: 네이버 sdk 로드

--- src/auth/social/naver.js
+++ src/auth/social/naver.js
@@ -1,5 +1,19 @@
-export const signInWithNaver = () => {
-  const naverLogin = new window.naver.LoginWithNaverId({
+const loadNaverSDK = () => {
+  return new Promise((resolve, reject) => {
+    if (window.naver) return resolve(window.naver);
+
+    const script = document.createElement("script");
+    script.src = "https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.2.js";
+    script.onload = () => resolve(window.naver);
+    script.onerror = reject;
+    document.head.appendChild(script);
+  });
+};
+
+export const signInWithNaver = async () => {
+  const naver = await loadNaverSDK();
+
+  const naverLogin = new naver.LoginWithNaverId({
     clientId: import.meta.env.VITE_NAVER_CLIENT_ID,
     callbackUrl: "https://light-way-hj-dps-projects.vercel.app/naver/callback",
     isPopup: true,

a231441f2d56d4caa797ab018f501db3181397a7
이호진 <skwo777@naver.com>
2025-03-28 02:52:25 +0900

    fix: url 설정

--- src/auth/social/naver.jsx
+++ src/auth/social/naver.jsx
@@ -17,7 +17,7 @@ export default function signInWithNaver() {
     loadNaverSDK().then(() => {
       const naverLogin = new window.naver.LoginWithNaverId({
         clientId: import.meta.env.VITE_NAVER_CLIENT_ID,
-        callbackUrl: "https://your.site/naver/callback",
+        callbackUrl: "https://light-way-hj-dps-projects.vercel.app/callback",
         isPopup: true,
         loginButton: { color: "green", type: 3, height: 40 },
       });

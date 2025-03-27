4c847dcf97db08004fbd03d12c62a68dda1949ac
이호진 <skwo777@naver.com>
2025-03-28 02:35:11 +0900

    fix: 에러 처리

--- src/auth/social/naver.js
+++ src/auth/social/naver.js
@@ -17,7 +17,6 @@ export const signInWithNaver = async () => {
     clientId: import.meta.env.VITE_NAVER_CLIENT_ID,
     callbackUrl: "https://light-way-hj-dps-projects.vercel.app/naver/callback",
     isPopup: true,
-    loginButton: { color: "green", type: 3, height: 40 },
   });
 
   naverLogin.init();
